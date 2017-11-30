3acb47f770c713a44e62a2933f4153dbdef754a4
tayler-harvey <taylerh_16@hotmail.com>
2017-11-30 16:15:55 -0400

    Creation of EditClubMain.
    Need Login and CreateClub use cases to continue.

--- Team11ProjectRepository/src/EditClubControl.java
+++ Team11ProjectRepository/src/EditClubControl.java
@@ -7,28 +7,21 @@ import java.util.HashMap;
 /** 
  * <!-- begin-UML-doc -->
  * <!-- end-UML-doc -->
- * @author dmccardl
+ * @author tharvey2
  * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
  */
 public class EditClubControl {
-	/** 
-	 * <!-- begin-UML-doc -->
-	 * <!-- end-UML-doc -->
-	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
-	private DataManager dataManager;
 
-	/** 
-	 * <!-- begin-UML-doc -->
-	 * <!-- end-UML-doc -->
-	 * @param clubName
-	 * @param formInfo
-	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
+	private DataManager dataManager;
+	
+	public EditClubControl(DataManager dmIn) {
+		dataManager = dmIn;
+	}
+	
 	public void processEditClub(String clubName, HashMap formInfo) {
 		// begin-user-code
 		// TODO Auto-generated method stub
-
+		
 		// end-user-code
 	}
 }
--- Team11ProjectRepository/src/EditClubUI.java
+++ Team11ProjectRepository/src/EditClubUI.java
@@ -5,41 +5,25 @@
 /** 
  * <!-- begin-UML-doc -->
  * <!-- end-UML-doc -->
- * @author dmccardl
- * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+ * @author tharvey2
  */
 public class EditClubUI {
-	/** 
-	 * <!-- begin-UML-doc -->
-	 * <!-- end-UML-doc -->
-	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
-	private EditClubControl editClubControl;
-	/** 
-	 * <!-- begin-UML-doc -->
-	 * <!-- end-UML-doc -->
-	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
-	private LoginUI loginUI;
 
-	/** 
-	 * <!-- begin-UML-doc -->
-	 * <!-- end-UML-doc -->
-	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
+	private EditClubControl editClubControl;
+	private LoginUI loginUI;
+	
+	
+	public void displayLoginRequestMessage() {
+	
+	}
+
 	public void displayEditClubForm() {
 		// begin-user-code
 		// TODO Auto-generated method stub
-
+		
 		// end-user-code
 	}
 
-	/** 
-	 * <!-- begin-UML-doc -->
-	 * <!-- end-UML-doc -->
-	 * @param result
-	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
 	public void displayConfirmation(boolean result) {
 		// begin-user-code
 		// TODO Auto-generated method stub
@@ -47,11 +31,6 @@ public class EditClubUI {
 		// end-user-code
 	}
 
-	/** 
-	 * <!-- begin-UML-doc -->
-	 * <!-- end-UML-doc -->
-	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
 	public void exit() {
 		// begin-user-code
 		// TODO Auto-generated method stub
@@ -59,11 +38,6 @@ public class EditClubUI {
 		// end-user-code
 	}
 
-	/** 
-	 * <!-- begin-UML-doc -->
-	 * <!-- end-UML-doc -->
-	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
 	public void enterInfo() {
 		// begin-user-code
 		// TODO Auto-generated method stub
