42dd205681d8f241e5241cb8b349fc4333e62f7c
Cameron Dahr <CameronSDahr@gmail.com>
2017-11-30 16:20:46 -0400

    ClubAdminRequest

---
+++ Team11ProjectRepository/src/ClubAdminServlet.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+/**
+ * Servlet implementation class ViewOrderStatusServlet
+ */
+@WebServlet("/ViewOrderStatusServlet")
+public class ViewOrderStatusServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ViewOrderStatusServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DataManager dm = new DataManager();
+        ViewOrderStatusControl control = new ViewOrderStatusControl(dm);
+        PrintWriter writer = response.getWriter();
+        ClubAdminAccountObject clubAdmin;
+        try {
+		HttpSession session=request.getSession(false); 
+        clubAdmin =(ClubAdminAccountObject) session.getAttribute("User");
+        }catch(Exception e){
+        	clubAdmin = null;
+        }
+		if (clubAdmin == null) {
+			 writer.println("You are not logged into a club admin account.<br>");
+			 writer.println("<p><a href=MainUI.html> Home </a> </p>");
+ 			writer.println("<p><a href=LoginUI.html> Login </a> </p>");
+		}else {
+			if (clubAdmin.club == null){
+				writer.println("<p><a href=CreateClubRequestForm.html> Create a Club Request <a/> </p>");
+			}else{
+				writer.println("<p><a href=EditClubForm.html> Edit your Club <a/> </p>");
+			}
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
