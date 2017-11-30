f7d04ff03222d36a72763946c2d5e24f13a05e7b
tayler-harvey <taylerh_16@hotmail.com>
2017-11-30 15:21:26 -0400

    Completed SearchClubsMain and SearchClubsUI

--- Team11ProjectRepository/src/SearchClubsControl.java
+++ Team11ProjectRepository/src/SearchClubsControl.java
@@ -11,21 +11,14 @@ import java.util.ArrayList;
  * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
  */
 public class SearchClubsControl {
-	/** 
-	 * <!-- begin-UML-doc -->
-	 * <!-- end-UML-doc -->
-	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
-	private DataManager dataManager;
 
-	/** 
-	 * <!-- begin-UML-doc -->
-	 * <!-- end-UML-doc -->
-	 * @param parameters
-	 * @return
-	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
-	public ArrayList processSearch(ArrayList parameters) {
+	private DataManager dataManager;
+	
+	public SearchClubsControl(DataManager dmIn) {
+		this.dataManager = dmIn;
+	}
+
+	public ArrayList<ClubObject> processSearch(ArrayList<String> parameters) {
 		// begin-user-code
 		// TODO Auto-generated method stub
 		return null;
--- Team11ProjectRepository/src/SearchClubsUI.java
+++ Team11ProjectRepository/src/SearchClubsUI.java
@@ -3,67 +3,66 @@
  */
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /** 
  * <!-- begin-UML-doc -->
  * <!-- end-UML-doc -->
- * @author dmccardl
- * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+ * @author tharvey2
  */
 public class SearchClubsUI {
-	/** 
-	 * <!-- begin-UML-doc -->
-	 * <!-- end-UML-doc -->
-	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
+
 	private SearchClubsControl searchClubsControl;
 
-	/** 
-	 * <!-- begin-UML-doc -->
-	 * <!-- end-UML-doc -->
-	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
+	public SearchClubsUI(SearchClubsControl controlIn) {
+		this.searchClubsControl = controlIn;
+	}
 	public void displaySearchForm() {
 		// begin-user-code
 		// TODO Auto-generated method stub
-
+		System.out.println("Enter one or more keywords and end with '0'.");
 		// end-user-code
 	}
 
-	/** 
-	 * <!-- begin-UML-doc -->
-	 * <!-- end-UML-doc -->
-	 * @param clubs
-	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
-	public void displayRetrievedClubs(ArrayList clubs) {
+	public void displayRetrievedClubs(ArrayList<ClubObject> clubs) {
 		// begin-user-code
 		// TODO Auto-generated method stub
-
+		if(clubs.size() == 0)
+			displayNoClubsFoundMessage();
+		else {
+			System.out.println("Club Search Results:");
+			for(int i = 0; i < clubs.size(); i++) {
+				System.out.println("Club name: " + clubs.get(i).name);
+				System.out.println("Description: " + clubs.get(i).description);
+				System.out.println("Location: " + clubs.get(i).location);
+				System.out.println("Club Leader: " + clubs.get(i).clubAdmin);
+			}
+		}
 		// end-user-code
 	}
 
-	/** 
-	 * <!-- begin-UML-doc -->
-	 * <!-- end-UML-doc -->
-	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
 	public void displayNoClubsFoundMessage() {
 		// begin-user-code
 		// TODO Auto-generated method stub
-
+		System.out.println("No clubs were found matching your search keywords.");
 		// end-user-code
 	}
 
-	/** 
-	 * <!-- begin-UML-doc -->
-	 * <!-- end-UML-doc -->
-	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
 	public void enterFilters() {
 		// begin-user-code
 		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<String> keywordList = new ArrayList<String>();
+		while (true) {
+			String keyword = scanner.next();
+			if(keyword.equals("0")) break;
+			keywordList.add(keyword);
+		}
+		scanner.close();
+		
+		ArrayList<ClubObject> club = searchClubsControl.processSearch(keywordList);
 
+		displayRetrievedClubs(club);
 		// end-user-code
 	}
 }
