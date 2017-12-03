6fafdfc43fd319477ef88d80f6bed2958a50589a
dmccardle <danny.mccardle@unb.ca>
2017-12-03 16:01:02 -0400

    Implemented DataManager Methods getAllClubRequests, getClubRequestbyID, setNewClub. Implemented processDisplayClubRequests and processApproval of CreateClubControl.

--- Team11ProjectRepository/src/CreateClubControl.java
+++ Team11ProjectRepository/src/CreateClubControl.java
@@ -11,51 +11,42 @@ import java.util.ArrayList;
  * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
  */
 public class CreateClubControl {
-	/** 
-	 * <!-- begin-UML-doc -->
-	 * <!-- end-UML-doc -->
-	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
+
 	private DataManager dataManager;
+	
+	public CreateClubControl(DataManager dm) {
+		this.dataManager = dm;
+	}
 
-	/** 
-	 * <!-- begin-UML-doc -->
-	 * <!-- end-UML-doc -->
-	 * @param clubInfo
-	 * @return
-	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
-	public boolean processApproval(Object clubInfo) {
+	public boolean processApproval(String requestID) {
+		// begin-user-code
+		// TODO Auto-generated method stub
+		ClubRequestObject request = dataManager.getClubRequestByID(requestID);
+		
+		ClubObject newClub = new ClubObject();
+		newClub.name = request.clubName;
+		newClub.description = request.description;
+		newClub.location = request.location;
+		newClub.clubAdmin = request.nameOfRequestSender;
+		
+		boolean result = dataManager.setNewClub(newClub);
+		
+		return result;
+		
+		// end-user-code
+	}
+
+	public boolean processRemoval(String requestID) {
 		// begin-user-code
 		// TODO Auto-generated method stub
 		return false;
 		// end-user-code
 	}
 
-	/** 
-	 * <!-- begin-UML-doc -->
-	 * <!-- end-UML-doc -->
-	 * @param suggestedClubName
-	 * @return
-	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
-	public boolean processRemoval(String suggestedClubName) {
+	public ArrayList<ClubRequestObject> processDisplayClubRequests() {
 		// begin-user-code
 		// TODO Auto-generated method stub
-		return false;
-		// end-user-code
-	}
-
-	/** 
-	 * <!-- begin-UML-doc -->
-	 * <!-- end-UML-doc -->
-	 * @return
-	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
-	public ArrayList processDisplayClubRequests() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
+		return dataManager.getAllClubRequests();
 		// end-user-code
 	}
 }
