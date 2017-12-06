cd520839b4c44bdfa3a05d72bb841fea02303844
tayler-harvey <taylerh_16@hotmail.com>
2017-12-06 11:51:10 -0400

    ClubAdminRequestObject constructor made to extend RequestObject

--- Team11ProjectRepository/src/ClubAdminRequestObject.java
+++ Team11ProjectRepository/src/ClubAdminRequestObject.java
@@ -7,7 +7,7 @@
  * <!-- end-UML-doc -->
  * @author tharvey2
  */
-public class ClubAdminRequestObject {
+public class ClubAdminRequestObject extends RequestObject {
 
 	public Object id;
 
@@ -26,10 +26,13 @@ public class ClubAdminRequestObject {
 	 * <!-- end-UML-doc -->
 	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	 */
-	public void sql() {
-		// begin-user-code
-		// TODO Auto-generated method stub
-
-		// end-user-code
+	public ClubAdminRequestObject(String requestId, String senderName, String idIn, String passwordIn, String firstNameIn, String lastNameIn, String emailIn, String clubNameIn) {
+		super(requestId, senderName);
+		this.id = idIn;
+		this.password = passwordIn;
+		this.firstName = firstNameIn;
+		this.lastName = lastNameIn;
+		this.email = emailIn;
+		this.clubName = clubNameIn;
 	}
 }
