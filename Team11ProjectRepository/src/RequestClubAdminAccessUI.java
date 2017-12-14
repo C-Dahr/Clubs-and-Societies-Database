ea624b22f0b94d6b330c3049aa4681edbeaa6e3a
tayler-harvey <taylerh_16@hotmail.com>
2017-12-14 15:05:22 -0400

    Created RequestClubAdminAccessMain, completed RequestClubAdminAccessUI

--- Team11ProjectRepository/src/RequestClubAdminAccessUI.java
+++ Team11ProjectRepository/src/RequestClubAdminAccessUI.java
@@ -1,11 +1,11 @@
 /**
  * 
  */
-
+import java.util.Scanner;
 /** 
  * <!-- begin-UML-doc -->
  * <!-- end-UML-doc -->
- * @author dmccardl
+ * @author tharvey2
  * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
  */
 public class RequestClubAdminAccessUI {
@@ -16,15 +16,41 @@ public class RequestClubAdminAccessUI {
 	 */
 	private RequestClubAdminAccessControl requestClubAdminAccessControl;
 
-	/** 
-	 * <!-- begin-UML-doc -->
-	 * <!-- end-UML-doc -->
-	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
+	public RequestClubAdminAccessUI(RequestClubAdminAccessControl control) {
+		 this.requestClubAdminAccessControl = control;
+	}
+	
+	
 	public void displayClubAdminForm() {
 		// begin-user-code
 		// TODO Auto-generated method stub
 
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("Enter first name: ");
+		 String firstName = sc.nextLine();
+		 System.out.println("Enter last name: ");
+		 String lastName = sc.nextLine();
+		 System.out.println("Enter a username: ");
+		 String id = sc.nextLine();
+		 System.out.println("Enter an account password: ");
+		 String password = sc.nextLine();
+		 System.out.println("Enter an email: ");
+		 String email = sc.nextLine();
+		 System.out.println("Enter suggested club name: ");
+		 String clubName = sc.nextLine();
+		 String senderName = firstName + " " + lastName;
+
+		 sc.close();
+		 
+		 ClubAdminRequestObject formInfo = new ClubAdminRequestObject("id", senderName, id, password, firstName, lastName, email, clubName);
+		 
+		 boolean result = requestClubAdminAccessControl.processFormSubmission(formInfo);
+		 if(result) {
+			 displayConfirmation();
+		 }
+		 else {
+			 displayErrorMessage();
+		 }
 		// end-user-code
 	}
 
@@ -34,22 +60,17 @@ public class RequestClubAdminAccessUI {
 	 * @param result
 	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	 */
-	public void displayConfirmation(boolean result) {
+	public void displayConfirmation() {
 		// begin-user-code
 		// TODO Auto-generated method stub
-
+		System.out.println("Request submitted successfully.");
 		// end-user-code
 	}
 
-	/** 
-	 * <!-- begin-UML-doc -->
-	 * <!-- end-UML-doc -->
-	 * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	 */
-	public void enterInfo() {
+	public void displayErrorMessage() {
 		// begin-user-code
 		// TODO Auto-generated method stub
-
+		System.out.println("Error submitting request.");
 		// end-user-code
 	}
 }
