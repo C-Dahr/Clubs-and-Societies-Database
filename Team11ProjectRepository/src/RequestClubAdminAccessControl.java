f706f4749fd86493542742940117c781bb69ba40
tayler-harvey <taylerh_16@hotmail.com>
2017-12-14 18:57:00 -0400

    RequestClubAdminAccessControl complete

--- Team11ProjectRepository/src/RequestClubAdminAccessControl.java
+++ Team11ProjectRepository/src/RequestClubAdminAccessControl.java
@@ -5,7 +5,7 @@
 /** 
  * <!-- begin-UML-doc -->
  * <!-- end-UML-doc -->
- * @author dmccardl
+ * @author tharvey2
  * @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
  */
 public class RequestClubAdminAccessControl {
